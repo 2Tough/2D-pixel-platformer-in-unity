1f480a50ed5e9a4d44c8f4f66be614a8ee5341db
Polaski <vainferro@gmail.com>
2023-08-12 15:11:47 +0300

    Platform bug fixed.

--- 2D platformer game/Assets/Scripts/StickToPlatform.cs
+++ 2D platformer game/Assets/Scripts/StickToPlatform.cs
@@ -4,22 +4,8 @@ using UnityEngine;
 
 public class StickToPlatform : MonoBehaviour { 
 
-    private void OnCollisionEnter2D(Collision2D collision) 
-    {
-        if (collision.gameObject.name == "Player")
-        {
-            collision.gameObject.transform.SetParent(transform);
-        }
-    }
-    
-    private void OnCollisionExit2D(Collision2D collision) {
-        if (collision.gameObject.name == "Player")
-        {
-            collision.gameObject.transform.SetParent(null);
-        }
-    }
 
-    private void onTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
@@ -27,7 +13,7 @@ public class StickToPlatform : MonoBehaviour {
         }
     }
 
-    private void onTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
