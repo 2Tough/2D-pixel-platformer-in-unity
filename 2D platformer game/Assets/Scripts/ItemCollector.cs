bd4bbeb7576dc4de5e1f29c817633aed3de5f081
Polaski <vainferro@gmail.com>
2023-08-12 10:23:36 +0300

    Added text count for collectibles.

--- 2D platformer game/Assets/Scripts/ItemCollector.cs
+++ 2D platformer game/Assets/Scripts/ItemCollector.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    private int kiwis = 0;
+    [SerializeField] private Text kiwiText;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Kiwi"))
         {
             Destroy(collision.gameObject);
+            kiwis++;
+            kiwiText.text = "Kiwis: " + kiwis;
         }
     }
 }
