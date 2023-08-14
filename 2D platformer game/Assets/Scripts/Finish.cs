282d30d0a28247e39bd7cb670cd9172f2a09b9a0
Polaski <vainferro@gmail.com>
2023-08-14 20:02:24 +0300

    Fixed bug with flag.
    Start menu and game over next.

--- 2D platformer game/Assets/Scripts/Finish.cs
+++ 2D platformer game/Assets/Scripts/Finish.cs
@@ -1,29 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private AudioSource finishSound;
+    private bool levelComplete = false;
 
-    void Start()
+    private void Start()
     {
         finishSound = GetComponent<AudioSource>();
     }
 
-    private void onTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Finish")
+        if (collision.gameObject.name == "Player" && !levelComplete)
         {
-            Debug.Log("It works");
             finishSound.Play();
-            //CompleteLevel();
+            levelComplete = true;
+            Invoke("CompleteLevel", 2f);
+            
         }
     }
 
-    //private void CompleteLevel()
-    //{
-        
-    //}
-
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
